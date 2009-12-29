78a492aaaf670d5e34836973d4b55dc9784795ff
V Sreekanth <sreeix@gmail.com>
2009-12-29 23:44:47 +0530

    Adding the bundler gemspec

--- meeting-ticker.rb
+++ meeting-ticker.rb
@@ -1,5 +1,21 @@
 require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+
+DataMapper::setup(:default, "sqlite://#{Dir.pwd}/meetings.db")
+
+class Meeting
+    include DataMapper::Resource
+    property :id, Serial
+    property :attendees_count, Integer
+    property :amount, String
+    property :hourly_rate, Float
+    property :duration, Float
+    property :created_at, DateTime
+end
+
+# automatically create the post table
+Meeting.auto_migrate! unless Meeting.table_exists?
 
 get '/' do
   erb :index
